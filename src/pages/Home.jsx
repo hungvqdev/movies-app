65d81c3e1eacaf7e221cc327d26d78764573c931
hungvqdev <hungvq.dev@gmail.com>
2022-10-13 02:37:04 +0700

    Hero Slide

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import HeroSlide from '../components/hero-slide/HeroSlide';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div><HeroSlide/></div>
   )
 }
 
